4f014a13ddff32f84a9fe2e8adeec501318f94de
kstansa <60517152+kstansa@users.noreply.github.com>
2024-12-17 11:17:35 -0800

    Improve pathfinding for casino fingerprint hack (#10)
    * Change fingerprint path-finding to BFS
    
    * Change algorithm for tracking path in search algorithm to prevent excessive array copying
    
    * Fix issue with directionality and allow for wrapping in x and y axes
    
    * Change how tab is appended to output list
    
    * Remove target count assertation
    
    * Remove stray debug logging
    
    ---------
    
    Co-authored-by: kstansa <kstansa@proton.me>

--- hacks/casinofingerprint.py
+++ hacks/casinofingerprint.py
@@ -3,6 +3,7 @@ import time
 import keyboard
 import numpy as np
 from PIL import Image, ImageGrab
+from collections import deque, namedtuple
 
 tofind = (950, 155, 1335, 685)
 
@@ -25,6 +26,62 @@ def is_in(img, subimg):
         return True
     return False
 
+def find_shortest_solution(target_coordinates):
+    Point = namedtuple('Point', ('x', 'y'))
+    ReverseLinkedNode = namedtuple("ReverseLinkedNode", ('value', 'prev_node', 'idx'))
+    rows, cols = 4, 2
+    directions = [(0, 1, 's'), (1, 0, 'd'), (0, -1, 'w'), (-1, 0, 'a')]  # (delta_x, delta_y, key)
+
+    target_coordinates = [p if isinstance(p, Point) else Point(*p) for p in target_coordinates]
+    target_mask = 0
+    for target in target_coordinates:
+        target_mask |= 1 << ((target.y * cols) + target.x)
+
+    # BFS initialization
+    current_pos = Point(0, 0)
+    visited_mask = 1
+    path_head: ReverseLinkedNode = ReverseLinkedNode(None, None, -1)
+    if current_pos in target_coordinates:
+        path_head = ReverseLinkedNode('return', path_head, 0)
+    queue = deque([(current_pos, visited_mask, path_head)])  # (current_position, visited_mask, path_head)
+
+    # loop until all points have been visited or a solution has been found
+    while len(queue) > 0:
+        current_pos, visited_mask, path_head = queue.popleft()
+
+        # if all target points are visited, return the final path
+        if visited_mask & target_mask == target_mask:
+            output_list = [None] * (path_head.idx + 1)
+            while path_head.idx >= 0:
+                output_list[path_head.idx] = path_head.value
+                path_head = path_head.prev_node
+            return output_list + ['tab']
+
+        # explore neighbors
+        for delta_x, delta_y, key in directions:
+            new_x, new_y = current_pos.x + delta_x, current_pos.y + delta_y
+            # correct for wrapping
+            if new_x == -1:
+                new_x, new_y = cols-1, new_y-1
+            elif new_x == cols:
+                new_x, new_y = 0, new_y+1
+            new_y = new_y % rows
+
+            next_pos = Point(new_x, new_y)
+            pos_mask = 1 << ((next_pos.y * cols) + next_pos.x)
+            next_visited_mask = visited_mask | pos_mask
+            # skip if visited
+            if visited_mask == next_visited_mask:
+                continue
+
+            next_path_head = ReverseLinkedNode(key, path_head, path_head.idx+1)
+            # if next_pos is a target point
+            if target_mask & pos_mask != 0:
+                next_path_head = ReverseLinkedNode('return', next_path_head, next_path_head.idx+1)
+            queue.append((next_pos, next_visited_mask, next_path_head))
+
+    raise Exception('No solution found')
+
 def main(bbox):
     print('[*] Casino Fingerprint')
     im = ImageGrab.grab(bbox)
@@ -39,27 +96,7 @@ def main(bbox):
     sub0_.close()
     im.close()
 
-    moves = []
-
-    x, y = 0, 0
-    for pos in togo:
-        while x != pos[0]:
-            if x > pos[0]:
-                x -= 1
-                moves.append("a")
-            else:
-                x += 1
-                moves.append("d")
-        
-        while y != pos[1]:
-            if y > pos[1]:
-                y -= 1
-                moves.append("w")
-            else:
-                y += 1
-                moves.append("s")
-        moves.append("return")
-    moves.append("tab")
+    moves = find_shortest_solution(togo)
 
     print('-', moves)
     for key in moves:
