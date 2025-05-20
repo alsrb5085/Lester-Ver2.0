05343ae94bceb818b2201b89f3311d9ab11e99b0
Justi <60735440+JU5TDIE@users.noreply.github.com>
2025-05-21 05:12:02 +0900

    Update cayovoltage.py
    Better variables names

--- hacks/cayovoltage.py
+++ hacks/cayovoltage.py
@@ -32,28 +32,28 @@ moves = {
     (0, 2, 1, 1, 2, 0): ['enter', 'w', 'return', 'enter', 'return', 'enter', 'return'] # (1-3) + (2-2) + (3-1) 
 }
 
-# objective numbers
-objectivenumber_height = [123, 137, 137, 154, 173, 173, 195] #objective numbers have same height
-objectivenumber_length_0 = [865, 849, 881, 865, 849, 881, 865] # first number
-objectivenumber_length_1 = [955, 939, 971, 955, 939, 971, 955] # second number
-objectivenumber_length_2 = [1043, 1029, 1061, 1043, 1029, 1061, 1043] # third number
+# target numbers
+target_number_height = [123, 137, 137, 154, 173, 173, 195] # target numbers have same height
+target_number_length_0 = [865, 849, 881, 865, 849, 881, 865] # first number
+target_number_length_1 = [955, 939, 971, 955, 939, 971, 955] # second number
+target_number_length_2 = [1043, 1029, 1061, 1043, 1029, 1061, 1043] # third number
 
 # left numbers
-leftnumber_length = [509, 495, 527, 509, 495, 527, 509] # left nubmers have same length
-leftnumber_height_0 = [271, 287, 287, 303, 323, 323, 343] # first number
-leftnumber_height_1 = [507, 522, 522, 540, 557, 557, 579] # second number
-leftnumber_height_2 = [741, 755, 755, 773, 791, 791, 813] # third number
+left_number_length = [509, 495, 527, 509, 495, 527, 509] # left nubmers have same length
+left_number_height_0 = [271, 287, 287, 303, 323, 323, 343] # first number
+left_number_height_1 = [507, 522, 522, 540, 557, 557, 579] # second number
+left_number_height_2 = [741, 755, 755, 773, 791, 791, 813] # third number
 
 # right symbols
-rightsymbol_length = [1351, 1349] # right symbols have same length
-rightsymbol_height_0 = [305, 277] # first symbol
-rightsymbol_height_1 = [541, 513] # second symbol
-rightsymbol_height_2 = [775, 747] # third symbol
+right_symbol_length = [1351, 1349] # right symbols have same length
+right_symbol_height_0 = [305, 277] # first symbol
+right_symbol_height_1 = [541, 513] # second symbol
+right_symbol_height_2 = [775, 747] # third symbol
 
-def pixel_check(x, y, img, maximum, dictionary):
+def pixel_check(x, y, img, dictionary):
     hints = []
 
-    for i in range(0, maximum):
+    for i in range(0, len(list(dictionary)[0])):
         pixel = img[y[i]:y[i] + 1, x[i]:x[i] + 1]
 
         if np.mean(pixel):
@@ -63,34 +63,14 @@ def pixel_check(x, y, img, maximum, dictionary):
             
     return dictionary[tuple(hints)]
 
-def objective_number(img):
-    value = (100 * (pixel_check(objectivenumber_length_0, objectivenumber_height, img, 7, DIGITS_LOOKUP))) + (10 * (pixel_check(objectivenumber_length_1, objectivenumber_height, img, 7, DIGITS_LOOKUP))) + pixel_check(objectivenumber_length_2, objectivenumber_height, img, 7, DIGITS_LOOKUP)
-    return value
-
-def left_numbers(img):
-    values = []
-    values.append(pixel_check(leftnumber_length, leftnumber_height_0, img, 7, DIGITS_LOOKUP))
-    values.append(pixel_check(leftnumber_length, leftnumber_height_1, img, 7, DIGITS_LOOKUP))
-    values.append(pixel_check(leftnumber_length, leftnumber_height_2, img, 7, DIGITS_LOOKUP))
-
-    return values
-
-def right_symbols(img):
-    values = []
-    values.append(pixel_check(rightsymbol_length, rightsymbol_height_0, img, 2, RIGHT_SYMBOLS))
-    values.append(pixel_check(rightsymbol_length, rightsymbol_height_1, img, 2, RIGHT_SYMBOLS))
-    values.append(pixel_check(rightsymbol_length, rightsymbol_height_2, img, 2, RIGHT_SYMBOLS))
-
-    return values
-
-def calculate(a, b, c):
+def calculate(target_number, left_numbers, right_numbers):
     try:
         for i in range(0, 6):
             keys = []
             keys.append(list(tuple(moves)[i]))
 
             for z, x, v, n, k, l in keys:
-                if (a == b[z] * c[x] + b[v] * c[n] + b[k] * c[l]):
+                if (target_number == left_numbers[z] * right_numbers[x] + left_numbers[v] * right_numbers[n] + left_numbers[k] * right_numbers[l]):
                     print('-', moves[tuple(moves)[i]])
                     for key in (moves[tuple(moves)[i]]):
                         keyboard.press_and_release(key)
@@ -110,12 +90,24 @@ def main(bbox):
     im = im.resize((1920,1080))
 
     grayImage = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2GRAY)
-    (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 120, 255, cv2.THRESH_BINARY)
+    (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 100, 255, cv2.THRESH_BINARY)
+    
+    target_number = (
+        (100 * (pixel_check(target_number_length_0, target_number_height, blackAndWhiteImage, DIGITS_LOOKUP)))
+    + (10 * (pixel_check(target_number_length_1, target_number_height, blackAndWhiteImage, DIGITS_LOOKUP)))
+    + pixel_check(target_number_length_2, target_number_height, blackAndWhiteImage, DIGITS_LOOKUP)
+    )
+    left_numbers = [
+        pixel_check(left_number_length, left_number_height_0, blackAndWhiteImage, DIGITS_LOOKUP),
+        pixel_check(left_number_length, left_number_height_1, blackAndWhiteImage, DIGITS_LOOKUP),
+        pixel_check(left_number_length, left_number_height_2, blackAndWhiteImage, DIGITS_LOOKUP)
+    ]
+    right_numbers = [
+        pixel_check(right_symbol_length, right_symbol_height_0, blackAndWhiteImage, RIGHT_SYMBOLS),
+        pixel_check(right_symbol_length, right_symbol_height_1, blackAndWhiteImage, RIGHT_SYMBOLS),
+        pixel_check(right_symbol_length, right_symbol_height_2, blackAndWhiteImage, RIGHT_SYMBOLS)
+    ]
 
-    objectivenumber = objective_number(blackAndWhiteImage)
-    leftnumbers = left_numbers(blackAndWhiteImage)
-    rightnumbers = right_symbols(blackAndWhiteImage)
+    print('- ', target_number, left_numbers, right_numbers)
 
-    print('- ', objectivenumber, leftnumbers, rightnumbers)
-
-    calculate(objectivenumber, leftnumbers, rightnumbers)
+    calculate(target_number, left_numbers, right_numbers)
